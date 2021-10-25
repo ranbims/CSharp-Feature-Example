584231ac5d6760b750de6c5b6fb8e460da1efff0
Ran <msnbiran@live.cn>
2021-10-25 22:39:48 +0800

    Ver2.0: Partial types

---
+++ Ver2.0/PartialTypes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PartialTypes
+{
+    // Ref: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/partial-classes-and-methods#partial-classes
+    class Program
+    {
+        public partial class Coords
+        {
+            private int x;
+            private int y;
+
+            public Coords(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        public partial class Coords
+        {
+            public void PrintCoords()
+            {
+                Console.WriteLine("Coords: {0},{1}", x, y);
+            }
+        }
+
+        // Output: Coords: 10,15
+        static void Main(string[] args)
+        {
+            Coords myCoords = new Coords(10, 15);
+            myCoords.PrintCoords();
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
