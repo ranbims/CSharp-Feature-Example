9b7fe6f2570f2bfb7f3ab78241ff1e471229c7e8
Ran <msnbiran@live.cn>
2021-10-25 12:39:45 +0800

    Ver1.0: Structs

---
+++ Ver1.0/Structs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        public struct Coords
+        {
+            public Coords(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public double X { get; }
+            public double Y { get; }
+
+            public override string ToString() => $"({X}, {Y})";
+        }
+
+        static void Main(string[] args)
+        {
+            Coords coords = new Coords(0.1d, 0.2d);
+            Console.WriteLine(coords);
+        }
+    }
+}
