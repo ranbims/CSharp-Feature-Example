87e04ef37f5cad9a360e295271d1dda5b04a7a1c
Ran <msnbiran@live.cn>
2021-10-25 22:44:21 +0800

    Ver2.0: Nullable value types

---
+++ Ver2.0/NullableValueTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NullableValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = 1;
+            int? b = null;
+            float? c = 0.1f;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
