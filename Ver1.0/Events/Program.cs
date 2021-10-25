3adda168ef70b157c90c788f678658ee7c7750b2
Ran <msnbiran@live.cn>
2021-10-25 13:02:45 +0800

    Ver1.0: Events

---
+++ Ver1.0/Events/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        public class CustomEventArgs : EventArgs
+        {
+            public string Message { get; }
+
+            public CustomEventArgs(string message)
+            {
+                Message = message;
+            }
+        }
+
+        public static event EventHandler<CustomEventArgs> EventHandler;
+
+        static void Main(string[] args)
+        {
+            EventHandler += Program_EventHandler_1;
+            EventHandler += Program_EventHandler_1;
+            EventHandler += Program_EventHandler_3;
+            EventHandler += Program_EventHandler_2;
+            EventHandler?.Invoke(new object(), new CustomEventArgs("Hello apple"));
+
+            EventHandler -= Program_EventHandler_1;
+            EventHandler -= Program_EventHandler_2;
+            EventHandler?.Invoke(new object(), new CustomEventArgs("Hello bear"));
+
+            EventHandler -= Program_EventHandler_1;
+            EventHandler -= Program_EventHandler_3;
+            EventHandler?.Invoke(new object(), new CustomEventArgs("Hello car"));
+        }
+
+        private static void Program_EventHandler_1(object sender, CustomEventArgs e)
+        {
+            Console.WriteLine("This is event hanlder 1, message: " + e.Message);
+        }
+
+        private static void Program_EventHandler_2(object sender, CustomEventArgs e)
+        {
+            Console.WriteLine("This is event hanlder 2, message: " + e.Message);
+        }
+
+        private static void Program_EventHandler_3(object sender, CustomEventArgs e)
+        {
+            Console.WriteLine("This is event hanlder 3, message: " + e.Message);
+        }
+    }
+}
