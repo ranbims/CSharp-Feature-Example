a6890fcc6058aba49be19a9c06e6d896c3b98cbf
Ran <msnbiran@live.cn>
2021-10-25 22:54:29 +0800

    Ver2.0: Iterators

---
+++ Ver2.0/Iterators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Iterators
+{
+    class Program
+    {
+        //Ref: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/iterators
+        static void Main()
+        {
+            int[] numbers = new int[] { 3, 5, 8 };
+            foreach (int number in numbers)
+            {
+                Console.Write(number.ToString() + " ");
+            }
+            // Output: 3 5 8
+            Console.ReadKey();
+        }
+    }
+}
