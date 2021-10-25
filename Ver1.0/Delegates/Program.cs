c664e3bfc3cb40c9904a241afaa469674d7ac6cf
Ran <msnbiran@live.cn>
2021-10-25 13:44:33 +0800

    Ver1.0: Delegates

---
+++ Ver1.0/Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate void Del(string message);
+
+        static void Main(string[] args)
+        {
+            // Instantiate the delegate. The v1.0 way.
+            Del handler = new Del(DelegateMethod);
+
+            // v2.0 way
+            // Del handler = DelegateMethod;
+
+            // Call the delegate.
+            handler("Hello World");
+        }
+
+        // Create a method for a delegate.
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
