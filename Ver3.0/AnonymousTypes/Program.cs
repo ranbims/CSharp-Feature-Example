cdffce597845022c79d01c9740a5608d892e5773
Ran <msnbiran@live.cn>
2021-10-26 12:45:08 +0800

    Ver3.0: Anonymous Types

---
+++ Ver3.0/AnonymousTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnonymousTypes
+{
+    // Ref: https://docs.microsoft.com/en-us/dotnet/csharp/fundamentals/types/anonymous-types
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = new { Amount = 108, Message = "Hello" };
+
+            // Rest the mouse pointer over v.Amount and v.Message in the following
+            // statement to verify that their inferred types are int and string.
+            Console.WriteLine(v.Amount + v.Message);
+        }
+    }
+}
