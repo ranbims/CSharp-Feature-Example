f234ef353a476c8c2f44168b9e5c0000e4745f8f
Ran <msnbiran@live.cn>
2021-10-26 13:13:01 +0800

    Ver3.0: Lambda

---
+++ Ver3.0/Lambda/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lambda
+{
+    class Program
+    {
+        // Ref: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/lambda-expressions
+        static void Main(string[] args)
+        {
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(5));
+            // Output:
+            // 25
+
+            System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+            Console.WriteLine(e);
+            // Output:
+            // x => (x * x)
+
+            Action<string> greet = name =>
+            {
+                string greeting = $"Hello {name}!";
+                Console.WriteLine(greeting);
+            };
+            greet("World");
+            // Output:
+            // Hello World!
+        }
+    }
+}
