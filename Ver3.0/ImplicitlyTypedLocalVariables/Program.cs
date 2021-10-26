6bf34049c51fbebd0484a0622d86e3dfda79f92d
Ran <msnbiran@live.cn>
2021-10-26 13:31:08 +0800

    Ver3.0: Implicitly typed local variables

---
+++ Ver3.0/ImplicitlyTypedLocalVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+// `var` could be used in ver3.0
+namespace ImplicitlyTypedLocalVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 10; // Implicitly typed.
+            var xs = new List<int>();
+        }
+    }
+}
