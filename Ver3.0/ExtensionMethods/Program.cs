8294bc1599f7dc43996e3317d4332bff926b434b
Ran <msnbiran@live.cn>
2021-10-26 13:26:00 +0800

    Ver3.0: Extension Methods

---
+++ Ver3.0/ExtensionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' },
+                             StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello Extension Methods";
+
+            // This is a call to extension method.
+            int i = s.WordCount();
+
+            Console.WriteLine(i);
+        }
+    }
+}
