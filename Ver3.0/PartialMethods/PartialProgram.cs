fd536eb5a9350cfa6f7cd24965a3b2d233f343a1
Ran <msnbiran@live.cn>
2021-10-26 13:44:09 +0800

    Ver3.0: Partial Methods

---
+++ Ver3.0/PartialMethods/PartialProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartialMethods
+{
+    partial class Program
+    {
+        partial void OnSomethingHappened(string s)
+        {
+            Console.WriteLine("Something happened: {0}", s);
+        }
+    }
+}
---
+++ Ver3.0/PartialMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// A partial method has its signature defined in one part of a partial type, 
+/// and its implementation defined in another part of the type. 
+/// 
+/// Partial methods enable class designers to provide method hooks, 
+/// similar to event handlers, that developers may decide to implement or not. 
+/// 
+/// If the developer does not supply an implementation, the compiler removes the signature at compile time.
+/// </summary>
+namespace PartialMethods
+{
+    partial class Program
+    {
+        partial void OnSomethingHappened(String s);
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.OnSomethingHappened("What happend?");
+        }
+    }
+}
