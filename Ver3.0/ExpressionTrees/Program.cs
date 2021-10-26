efaea31db1387b1d17c29bb175da37ab99708138
Ran <msnbiran@live.cn>
2021-10-26 13:22:04 +0800

    Ver3.0: ExpressionTree(Todo)

---
+++ Ver3.0/ExpressionTrees/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// This concept is a little bit hard to understand. Recommned to read the full introductions below.
+// https://docs.microsoft.com/en-us/dotnet/csharp/expression-trees
+namespace ExpressionTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Todo");
+        }
+    }
+}
