8b2c9e79a5e0dd3f9cb7c94c998649eb0ebde084
Ran <msnbiran@live.cn>
2021-10-26 13:08:17 +0800

    Ver3.0: LINQ

---
+++ Ver3.0/LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    // Ref: https://docs.microsoft.com/en-us/dotnet/csharp/linq/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source.
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+            // Define the query expression.
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            // Execute the query.
+            foreach (int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
+            // Output: 97 92 81
+        }
+    }
+}
