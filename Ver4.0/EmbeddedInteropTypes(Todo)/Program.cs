1ef046ace1fb24c9a4591eb76e092b2883b3929b
Ran <msnbiran@live.cn>
2021-10-27 14:29:33 +0800

    Ver4.0: EmbeddedInteropTypes(Todo)

---
+++ Ver4.0/EmbeddedInteropTypes(Todo)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmbeddedInteropTypes_Todo_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a todo topic.");
+        }
+    }
+}
