f9b1f780fed4e226758d8b28742dee5809353ba7
Ran <msnbiran@live.cn>
2021-10-27 14:27:04 +0800

    Ver4.0: Generic coviarance and contravariance

--- Ver2.0/CovarianceAndContravariance/Program.cs
+++ Ver2.0/CovarianceAndContravariance/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace CovarianceAndContravariance
+﻿namespace CovarianceAndContravariance
 {
     // Ref: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/covariance-contravariance/
     class Program
@@ -15,25 +12,10 @@ namespace CovarianceAndContravariance
             // An object of a more derived type is assigned to an object of a less derived type.
             object obj = str;
 
-            // Covariance.
-            IEnumerable<string> strings = new List<string>();
-            // An object that is instantiated with a more derived type argument
-            // is assigned to an object instantiated with a less derived type argument.
-            // Assignment compatibility is preserved.
-            IEnumerable<object> objects = strings;
-
-            // Contravariance.
-            // Assume that the following method is in the class:
-            // static void SetObject(object o) { }
-            Action<object> actObject = SetObject;
-            // An object that is instantiated with a less derived type argument
-            // is assigned to an object instantiated with a more derived type argument.
-            // Assignment compatibility is reversed.
-            Action<string> actString = actObject;
-
-            actObject(new object());
-            // actString(new object()); will meet a compile error.
-            actString(new string(""));
+            // this will meet a compile error.
+            //acceptString(obj);
         }
+
+        static void acceptString(string str) { }
     }
 }
---
+++ Ver4.0/GenericCovariantAndContravariant/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCovariantAndContravariant
+{
+    // Ref: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/covariance-contravariance/
+    class Program
+    {
+        static void SetObject(object o) { }
+
+        static void Main(string[] args)
+        {
+            // Assignment compatibility.
+            string str = "test";
+            // An object of a more derived type is assigned to an object of a less derived type.
+            object obj = str;
+
+            // Covariance.
+            IEnumerable<string> strings = new List<string>();
+            // An object that is instantiated with a more derived type argument
+            // is assigned to an object instantiated with a less derived type argument.
+            // Assignment compatibility is preserved.
+            IEnumerable<object> objects = strings;
+
+            // Contravariance.
+            // Assume that the following method is in the class:
+            // static void SetObject(object o) { }
+            Action<object> actObject = SetObject;
+            // An object that is instantiated with a less derived type argument
+            // is assigned to an object instantiated with a more derived type argument.
+            // Assignment compatibility is reversed.
+            Action<string> actString = actObject;
+
+            actObject(new object());
+            // actString(new object()); will meet a compile error.
+            actString(new string(""));
+        }
+    }
+}
