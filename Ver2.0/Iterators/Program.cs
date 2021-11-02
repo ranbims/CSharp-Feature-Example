70dee2220881a801d280697d102149abf0754992
Ran <msnbiran@live.cn>
2021-11-02 21:31:52 +0800

    Add example for Ver2.0: iterators

--- Ver2.0/Iterators/Program.cs
+++ Ver2.0/Iterators/Program.cs
@@ -14,6 +14,25 @@ namespace Iterators
             }
             // Output: 3 5 8
             Console.ReadKey();
+
+            foreach (int number in EvenSequence(5, 18))
+            {
+                Console.Write(number.ToString() + " ");
+            }
+            // Output: 6 8 10 12 14 16 18
+            Console.ReadKey();
+        }
+
+        public static System.Collections.Generic.IEnumerable<int> EvenSequence(int firstNumber, int lastNumber)
+        {
+            // Yield even numbers in the range.
+            for (int number = firstNumber; number <= lastNumber; number++)
+            {
+                if (number % 2 == 0)
+                {
+                    yield return number;
+                }
+            }
         }
     }
 }
