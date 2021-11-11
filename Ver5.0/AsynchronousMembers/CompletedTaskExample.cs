6644fee14d7863394a075ffc6c87ed54b2602b06
Ran Bi <ranbi@microsoft.com>
2021-11-11 14:42:36 +0800

    Ver5.0: Asynchronous members: Add more examples

---
+++ Ver5.0/AsynchronousMembers/CompletedTaskExample.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousMembers
+{
+    // Ref: https://stackoverflow.com/questions/44096253/await-task-completedtask-for-what
+    public class CompletedTaskExample
+    {
+        public class SampleModel
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+        }
+
+        public async Task<IEnumerable<SampleModel>> GetDataAsync()
+        {
+            await Task.CompletedTask; // <-- CompletedTaskExample
+            var data = new List<SampleModel>();
+
+            data.Add(new SampleModel
+            {
+                Title = "Lorem ipsum dolor sit 1",
+                Description = "Lorem ipsum dolor sit amet"
+            });
+
+            data.Add(new SampleModel
+            {
+                Title = "Lorem ipsum dolor sit 2",
+                Description = "Lorem ipsum dolor sit amet"
+            });
+            return data;
+        }
+
+        // An equivalent way
+        public async Task<IEnumerable<SampleModel>> GetDataAsyncEqu()
+        {
+            var data = new List<SampleModel>();
+
+            data.Add(new SampleModel
+            {
+                Title = "Lorem ipsum dolor sit 1",
+                Description = "Lorem ipsum dolor sit amet"
+            });
+
+            data.Add(new SampleModel
+            {
+                Title = "Lorem ipsum dolor sit 2",
+                Description = "Lorem ipsum dolor sit amet"
+            });
+            return await Task.FromResult<IEnumerable<SampleModel>>(data);
+        }
+    }
+}
---
+++ Ver5.0/AsynchronousMembers/TaskIdExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousMembers
+{
+    public class TaskIdExample
+    {
+        // Test Task static API.
+        public static async Task testTaskApiAsync()
+        {
+            Console.WriteLine(Task.CurrentId);
+            Task task1 = new Task(async () =>
+            {
+                Console.WriteLine(Task.CurrentId);
+                for (int i = 0; i < 10; i++)
+                {
+                    await Task.Delay(100);
+                }
+            });
+
+            Task task2 = new Task(async () =>
+            {
+                Console.WriteLine(Task.CurrentId);
+                for (int i = 0; i < 1000; i++)
+                {
+                    await Task.Delay(100);
+                }
+            });
+
+            task1.Start();
+            task2.Start();
+
+            for (int i = 0; i < 2000; i++)
+            {
+                Task task = new Task(() => { });
+                task.Start();
+                await task;
+                Console.WriteLine(Task.CurrentId);
+                await Task.Delay(100);
+            }
+        }
+    }
+}
