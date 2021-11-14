aa8b3bee37846cb09e904d8ab70af2769c90cae1
Ran <msnbiran@live.cn>
2021-11-14 23:46:48 +0800

    Ver10.0: Lambda expression improvements

---
+++ Ver10.0/LambdaExpressionImprovements/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static Attributes.Program;
+
+// Natural type
+var parse = (string s) => int.Parse(s);
+
+// Declared return type
+var choose = object (bool b) => b ? 1 : "two"; // Func<bool, object>
+
+// Attributes can be applied to lambda expressions
+var printAuthor = [Author("Ran Bi")] () => Console.WriteLine("author");
